441643587ced561560252a5df1eef4a45872553d
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-06-25 10:57:14 +0200

    fdasdfsasd

--- src/main/kotlin/no/fdk/userapi/service/SkattService.kt
+++ src/main/kotlin/no/fdk/userapi/service/SkattService.kt
@@ -2,14 +2,17 @@ package no.fdk.userapi.service
 
 import no.fdk.userapi.configuration.SkattProperties
 import no.fdk.userapi.model.RoleFDK
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class SkattService(
     private val skattProperties: SkattProperties
 ) {
+    private val logger = LoggerFactory.getLogger(SkattService::class.java)
 
     fun getAuthorities(groups: List<String>): String {
+        logger.info("groups: ${groups.joinToString(", ")}")
         val role = when {
             groups.contains(skattProperties.adminGroupID) -> RoleFDK.Role.Admin
             groups.contains(skattProperties.writeGroupID) -> RoleFDK.Role.Write
